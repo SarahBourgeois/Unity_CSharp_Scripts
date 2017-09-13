0ed3c3a4b837033c236b0d158a2e97e69339fb16
Sarah-Bourgeois <sarah@nudgeapm.io>
2017-09-13 21:49:06 +0200

    add touchscreen script for simple rotation

---
+++ TouchScreen_SimpleRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningCube : MonoBehaviour 
+{
+	public float m_Speed = 60f; // speed rotation of the cube
+	private Vector3 RotationDirection = Vector3.up; // vector init
+
+
+	public void ToggleRotationDirection()
+	{
+		if (RotationDirection == Vector3.up) 
+		{
+			RotationDirection = Vector3.down;
+		}
+		else 
+		{
+			RotationDirection = Vector3.up;
+		}
+
+	}
+
+	void Update() 
+	{
+		transform.Rotate(RotationDirection * Time.deltaTime * m_Speed);
+	}
+}
